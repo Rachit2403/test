5b7acc3730f11a04d391b2a01aec2f819fbd9886
Rachit Vyas <rachitvyas03@gmail.com>
2026-07-17 22:01:36 +0530

    Committed before college

---
+++ Python/strings/indexing.py
@@ -0,0 +1,6 @@
+#indexing = accessing the specified/restricted contents of a sequence using []
+# [start : end : step]
+#The 'end' digit is exclusive.
+cred_num = input("Enter your card number: ")
+true_num = cred_num.replace("-", "")
+print(true_num [12 : ])
