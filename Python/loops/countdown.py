5da8826e2cd655a2bb7972c20f3753ca201fb572
Rachit Vyas <rachitvyas03@gmail.com>
2026-07-18 09:39:54 +0530

    Redid the countdown, and revised the work done till now.

--- Python/loops/countdown.py
+++ Python/loops/countdown.py
@@ -1,11 +1,22 @@
+# instead of typecasting as integer, use "//" instead of just "/".
+
 import time
-my_time = int(input("Enter time in seconds: "))
-for x in range (my_time, 0, -1):
+duration = (input("Enter the duration of countdown in seconds: "))
+tim = int(duration)
+while tim <=0:
+    print("Time can't be negative or zero, dumbo!")
+    duration = int(input("Enter a possible value of time in seconds: "))
+
+    print("Please enter a valid duration!")
+    duration = int(input("Enter a valid duration of countdown in seconds: "))
+while tim < 1:
+    print("Enter a longer duration!")
+    duration = int(input("Enter a valid duration of countdown in seconds: "))
+    tim = int(duration)
+for x in range(tim, 0, -1):
     seconds = x % 60
-    minutes = int(x / 60) % 60
-    hours = int(x / 3600)
+    minutes = int(x // 60) % 60
+    hours = int(x // 3600)
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
-print("TIME'S UP!!!")
-
-# instead of typecasting as integer, us "//" instead of just "/".
+print("Time's up!")
--- Python/loops/nested_loops.py
+++ Python/loops/nested_loops.py
@@ -1,8 +1,17 @@
 rows = int(input("Enter the number of rows: "))
+while rows <= 0:
+    print("Please enter a positive number!")
+    rows = int(input("Enter the number of rows: "))
 columns = int(input("Enter the number of columns: "))
-symbol = input("Enter the symbol required: ")
+while columns <= 0:
+    print("Please enter a positive number!")
+    columns = int(input("Enter the number of columns: "))
 
+symbol = input("Enter the symbol required: ")
+while not len(symbol) == 1:
+    print("Please enter a single character!")
+    symbol = input("Enter the symbol required: ")   
 for x in range (rows):
     for y in range (columns):
-        print(symbol, end="R|")
+        print(symbol, end="")
     print()
